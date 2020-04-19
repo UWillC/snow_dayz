aa5cea1ffe4422f74404e2daa0f986f17bf5df7d
UWillC <UWillC@users.noreply.github.com>
2020-04-19 12:30:05 +0200

    Create __init__.py

---
+++ HUNTER/BEC/Plugins/PlayerCount/__init__.py
@@ -0,0 +1,41 @@
+'''
+    File name: __init__.py
+    Author: /u/robbychampagne, ThePyrotechnic
+    Date created: 9/29/2018
+    Date last modified: 9/29/2018
+'''
+from twisted.internet import task
+
+CONFIG = {
+    'interval': 5,
+    'message_singular': 'There is one player online',
+    'base_message': 'There are {} players online'
+}
+
+
+class RobbysPlayerPlugin:
+    def __init__(self, instance):
+        self.bec = instance
+        self.player_count_task = task.LoopingCall(self.send_player_count)
+        self.player_count_task.start(CONFIG.get('interval') * 60, False)
+
+    def get_players(self):
+        return self.bec.Bec_playersconnected
+
+    def send_player_count(self):
+        player_count = len(self.get_players())
+        if player_count == 0:
+            return
+
+        elif player_count == 1:
+            message = CONFIG.get('message_singular')
+
+        else:
+            message = CONFIG.get('base_message').format(player_count)
+
+        command_to_fire = 'say -1 {0}'.format(message)
+        self.bec._Bec_queuelist.append(command_to_fire)
+
+
+def start(instance):
+    RobbysPlayerPlugin(instance)
